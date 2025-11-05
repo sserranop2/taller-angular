18973131220efdb63987773b5dc13737302d5e7d
sserranop2 <tucorreo@example.com>
2025-11-05 10:58:30 -0500

    finalizado

--- mynewapp/src/app/app-module.ts
+++ mynewapp/src/app/app-module.ts
@@ -4,7 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { SerieModule } from './serie/serie.module';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+import { HttpErrorInterceptorService } from './interceptors/http-error-interceptor.service';
 
 @NgModule({
   declarations: [
@@ -14,10 +16,19 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     SerieModule,
-    HttpClientModule
+    HttpClientModule,
+    ToastrModule.forRoot({
+      timeOut: 10000,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+    }),
   ],
   providers: [
-    provideBrowserGlobalErrorListeners()
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpErrorInterceptorService,
+      multi: true,
+    },
   ],
   bootstrap: [App]
 })
---
+++ mynewapp/src/app/interceptors/http-error-interceptor.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed, inject } from '@angular/core/testing';
+import { ToastrModule } from 'ngx-toastr';
+import { HttpErrorInterceptorService } from './http-error-interceptor.service';
+
+describe('Service: HttpErrorInterceptor', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ToastrModule.forRoot()],
+      providers: [HttpErrorInterceptorService],
+    });
+  });
+
+  it('should ...', inject([HttpErrorInterceptorService], (service: HttpErrorInterceptorService) => {
+    expect(service).toBeTruthy();
+  }));
+});
